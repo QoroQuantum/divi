cff73ecd9caa50e784f8a155e006f597a911248f
Shiro-Raven <exclass9.24@gmail.com>
2024-11-25 17:47:30 +0100

    `setup.py` dependent on `requirements.txt`

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import find_packages, setup
 
+with open("requirements.txt") as f:
+    reqs = f.read().split("\n")
+
+with open("requirements_dev.txt") as f:
+    reqs_dev = f.read().split("\n")
+
 setup(
-    name='divi',
-    version='0.1',
+    name="divi",
+    version="0.1",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[],  # Add your requirements here if necessary
+    install_requires=reqs,
+    extras_require={"graph": ["PyMetis==2023.1.1"], "dev": reqs_dev},
 )
