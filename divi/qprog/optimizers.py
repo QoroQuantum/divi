1053048266ff696712dcb5e5a7ce3475a5f9dc4b
Shiro-Raven <exclass9.24@gmail.com>
2025-02-12 02:38:05 +0100

    Add layer option to VQE

--- divi/qprog/optimizers.py
+++ divi/qprog/optimizers.py
@@ -23,24 +23,27 @@ class Optimizers(Enum):
         return 1
 
     def compute_new_parameters(self, params, iteration, **kwargs):
-        if self == Optimizers.MONTE_CARLO:
-            losses = kwargs.pop("losses")
-            smallest_energy_keys = sorted(losses, key=lambda k: losses[k])[
-                : self.samples()
-            ]
-            new_params = []
-            for key in smallest_energy_keys:
-                new_param_set = [
-                    np.random.normal(
-                        params[int(key)],
-                        1 / (2 * iteration),
-                        size=params[int(key)].shape,
-                    )
-                    for _ in range(self.n_param_sets)
-                ]
-                for new_param in new_param_set:
-                    new_param = np.clip(new_param, 0, 2 * np.pi)
-                new_params.extend(new_param_set)
-            return new_params
-        else:
+        if self != Optimizers.MONTE_CARLO:
             raise NotImplementedError
+
+        losses = kwargs.pop("losses")
+        smallest_energy_keys = sorted(losses, key=lambda k: losses[k])[: self.samples()]
+
+        new_params = []
+
+        for key in smallest_energy_keys:
+            new_param_set = [
+                np.random.normal(
+                    params[int(key)],
+                    1 / (2 * iteration),
+                    size=params[int(key)].shape,
+                )
+                for _ in range(self.n_param_sets)
+            ]
+
+            for new_param in new_param_set:
+                new_param = np.clip(new_param, 0, 2 * np.pi)
+
+            new_params.extend(new_param_set)
+
+        return new_params
