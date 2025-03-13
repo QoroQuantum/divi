43a1898f5758c44372d6a49e67c76e8eff24e4ad
Stephen DiAdamo <stephen.diadamo@gmail.com>
2025-03-13 13:12:51 +0100

    Remove local logic for Usher URL

--- divi/services/qoro_service.py
+++ divi/services/qoro_service.py
@@ -7,8 +7,8 @@ from http import HTTPStatus
 
 import requests
 
-LOCAL = os.environ.get("LOCAL", "True") == "True"
-API_URL = "http://127.0.0.1:8000/api" if LOCAL else "https://app.qoroquantum.net/api"
+
+API_URL = "https://app.qoroquantum.net/api"
 
 
 class JobStatus(Enum):
