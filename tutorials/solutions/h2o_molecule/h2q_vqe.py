07bb2029cbe8e66721e06ef5179461fbd4506a6d
d93114 <leena.tharwat@d-fine.com>
2025-11-29 12:08:58 +0100

    task-description

---
+++ tutorials/solutions/h2o_molecule/h2q_vqe.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: 2025 Qoro Quantum Ltd <divi@qoroquantum.de>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import time
+
+import numpy as np
+import pennylane as qml
+
+from divi.backends import ParallelSimulator
+from divi.qprog import VQE, HartreeFockAnsatz
+from divi.qprog.optimizers import ScipyMethod, ScipyOptimizer
+
+if __name__ == "__main__":
+    mol = qml.qchem.Molecule(
+        symbols=["H", "H"], coordinates=np.array([(0, 0, 0), (0, 0, 0.5)])
+    )
+
+    optim = ScipyOptimizer(method=ScipyMethod.L_BFGS_B)
+
+    vqe_problem = VQE(
+        molecule=mol,
+        ansatz=HartreeFockAnsatz(),
+        n_layers=1,
+        optimizer=optim,
+        max_iterations=3,
+        backend=ParallelSimulator(),
+    )
+
+    t1 = time.time()
+
+    vqe_problem.run()
+
+    print(f"Minimum Energy Achieved: {vqe_problem.best_loss:.4f}")
+    print(f"Eigenstate: {vqe_problem.eigenstate}")
+    print(f"Total circuits: {vqe_problem.total_circuit_count}")
+    print(f"Time taken: {round(time.time() - t1, 5)} seconds")
